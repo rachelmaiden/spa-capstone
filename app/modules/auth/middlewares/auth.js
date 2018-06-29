56f8207ea794ded479b6a7535e64d92d9d389a5d
Crisaldo <crisaldo.santos.22@gmail.com>
2018-06-29 19:38:41 +0800

    check this

--- app/modules/auth/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-exports.hasAuth = (req, res, next) => {
-    if (req.session && req.session.user && Object.keys(req.session.user).length > 0) return next();
-    return res.redirect('/login?unauthorized');
-}
-
-exports.noAuthed = (req, res, next) => {
-    if (req.session && req.session.user && Object.keys(req.session.user).length > 0) return res.redirect('/');
-    return next();
-}
--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var router = express.Router()
+var db = require('../../lib/database')();
+
+
 
 router.get('/', (req, res) => {
     res.render('home/views/admindashboard')
@@ -8,26 +11,148 @@ router.get('/', (req, res) => {
 router.get('/adminAmenities', (req, res) => {
     res.render('home/views/adminAmenities')
 })
+// ***************************************** C U S T O M E R *****************************************
 
+ // READ 
 router.get('/adminCustomer', (req, res) => {
-    res.render('home/views/adminCustomer')
-
+    const query = ` select * from customer_tbl`
+    db.query(query,(err,out) =>{
+      res.render('home/views/adminCustomer',{
+        customers: out
+      })
+      console.log(out)
+    })
 })
 
+// CREATE(ADD)
+router.post('/adminCustomer',(req, res) => {
+  const query = `
+    insert into 
+    customer_tbl(cust_firstname,cust_middlename, cust_lastname, house_no,street_name,brgy_district,city, contact_no, cust_email, age) 
+    values("${req.body.firstname}","${req.body.middlename}","${req.body.lastname}", 
+      "${req.body.house_no}","${req.body.street_name}","${req.body.brgy_district}","${req.body.city}", "${req.body.contact_no}","${req.body.email}","${req.body.age}")
+        `
+  db.query(query, (err, out) => {
+    res.redirect('/adminCustomer')
+    console.log(query)
+  })
+})
+
+// DELETE
+router.post('/adminCustomer/delete', (req, res) => {
+    console.log(req.body.id)
+    const query = `delete from customer_tbl where cust_id = ${req.body.id}`
+      
+    db.query(query,(err,out)=>{
+      res.redirect('/adminCustomer')
+    })
+  })
+
+// ***************************************** P R O M O  B U N D L E *****************************************
+//READ
 router.get('/adminPromos', (req, res) => {
-    res.render('home/views/adminPromos')
-
+    const query = ` select * from promo_bundle_tbl;
+    select * from services_tbl `
+    db.query(query,(err,out) =>{
+      res.render('home/views/adminPromos',{
+        promos: out[0],
+        services: out[1]
+      })
+      console.log(out[0])
+      console.log(out[1])
+    })
 })
+// CREATE(ADD)
+router.post('/adminPromos',(req, res) => {
+    const query = `
+      insert into 
+      promo_bundle_tbl(promobundle_name, services_included, duration, promobundle_price,promobundle_valid)
+      value("${req.body.name}","${req.body.promos}","${req.body.duration}","${req.body.price}","${req.body.valid}")`
+    db.query(query, (err, out) => {
+      res.redirect('/adminPromos')
+      console.log(query)
+    })
+  })
 
+// DELETE
+router.post('/adminPromos/delete', (req, res) => {
+    console.log(req.body.id)
+    const query = `delete from promo_bundle_tbl where promobundle_id = ${req.body.id}`
+      
+    db.query(query,(err,out)=>{
+      res.redirect('/adminPromos')
+    })
+  }) 
+// ***************************************** R O O M *****************************************
+//READ
 router.get('/adminRooms', (req, res) => {
-    res.render('home/views/adminRooms')
-
+    const query = ` select * from room_tbl `
+    db.query(query,(err,out) =>{
+      res.render('home/views/adminRooms',{
+        rooms: out
+      })
+      console.log(out)
+    })
 })
+// CREATE(ADD)
+router.post('/adminRooms',(req, res) => {
+    const query = `
+      insert into 
+      room_tbl(room_name, room_type,bed_qty)
+      value("${req.body.name}","${req.body.type}","${req.body.quantity}")`
+    db.query(query, (err, out) => {
+      res.redirect('/adminRooms')
+      console.log(query)
+    })
+  })
+// DELETE
+router.post('/adminRooms/delete', (req, res) => {
+    console.log(req.body.id)
+    const query = `delete from room_tbl where room_id = ${req.body.id}`
+      
+    db.query(query,(err,out)=>{
+      res.redirect('/adminRooms')
+    })
+  })
 
+
+// ***************************************** S E R V I C E *****************************************
+//READ
 router.get('/adminServices', (req, res) => {
-    res.render('home/views/adminServices')
-
+    const query = ` select services_tbl.*, service_type.* from services_tbl, service_type 
+    where 
+    services_tbl.service_type = service_type.service_type_id`
+    db.query(query,(err,out) =>{
+      res.render('home/views/adminServices',{
+        services: out
+      })
+      console.log(out)
+    })
 })
+// CREATE(ADD)
+router.post('/adminServices',(req, res) => {
+    const query = `
+      insert into 
+      services_tbl(service_name, service_type, service_duration, service_price)
+      value("${req.body.name}","${req.body.type}","${req.body.duration}","${req.body.price}")`
+    db.query(query, (err, out) => {
+      res.redirect('/adminServices')
+      console.log(query)
+    })
+  })
+
+// DELETE
+router.post('/adminServices/delete', (req, res) => {
+    console.log(req.body.id)
+    const query = `delete from services_tbl where service_id = ${req.body.id}`
+      
+    db.query(query,(err,out)=>{
+      res.redirect('/adminServices')
+    })
+  })
+
+
+
 
 router.get('/adminTherapist', (req, res) => {
     res.render('home/views/adminTherapist')
