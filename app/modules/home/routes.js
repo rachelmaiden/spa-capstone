4921ccfc65da9dea574aad918783725696b60533
Crisaldo <crisaldo.santos.22@gmail.com>
2018-07-13 23:42:02 +0800

    5th push
    July 13 2018
    Hindi pa maayos yung update ng promo

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -4,13 +4,12 @@ var db = require('../../lib/database')();
 
 
 
-router.get('/admindashboard', (req, res) => {
-    res.render('home/views/admindashboard')
-})
 
 router.get('/adminAmenities', (req, res) => {
-    res.render('home/views/adminAmenities')
+  res.render('home/views/adminAmenities')
 })
+
+
 
 router.get('/adminQueue', (req, res) => {
   res.render('home/views/adminQueue')
@@ -18,18 +17,52 @@ router.get('/adminQueue', (req, res) => {
 
 router.get('/bookService', (req, res) => {
   const query = ` select * from services_tbl where service_type ='1';
-      select * from services_tbl where service_type ='2';
-      select * from services_tbl where service_type ='3'`
-      db.query(query,(err,out) =>{
-        res.render('home/views/bookService',{
-          bodys: out[0],
-          scrubs: out[1],
-          adds: out[2]
-        })
-        console.log(out)
-      })
+  select * from services_tbl where service_type ='2';
+  select * from services_tbl where service_type ='3'`
+  db.query(query,(err,out) =>{
+    res.render('home/views/bookService',{
+      bodys: out[0],
+      scrubs: out[1],
+      adds: out[2]
     })
+    console.log(out)
+  })
+})
+// ****************************************************************************************************************************************************************************************** ||
+// ****************************************************************************************************************************************************************************************** ||
+// ****************************************************************************************************************************************************************************************** ||
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  F R O N T  D E S K - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -||
+// ========================================================================================================================================================================================== ||
+// ========================================================================================================================================================================================== ||
+// ========================================================================================================================================================================================== ||
+router.get('/adminFrontDesk', (req, res) => {
+  res.render('home/views/adminFrontDesk')
+})
 
+router.post("/adminFrontDesk",(req, res) => {
+	const query = `
+		select * from admin_tbl where admin_username = "${req.body.admin_username}" and admin_desc="front_desk"`
+
+		db.query(query, (err, out) => {
+		console.log(out[0])
+ 		if(!out[0])
+			return res.redirect("/adminFrontDesk?notfound?")
+		else {
+			if(out[0].admin_password !== req.body.admin_password)
+				return res.redirect("/adminFrontDesk?incorrect")
+			else {
+				delete out[0].admin_password
+				req.session.user = out[0]	
+				return res.redirect("/admindashboard")
+			}
+		}
+
+	})
+})
+
+router.get('/admindashboard', (req, res) => {
+    res.render('home/views/admindashboard')
+})
 
 
 
@@ -53,13 +86,14 @@ router.get('/bookService', (req, res) => {
     router.post('/adminCustomer',(req, res) => {
       const query = `
         insert into 
-        customer_tbl(cust_firstname,cust_middlename, cust_lastname, house_no,street_name,brgy_district,city, contact_no, cust_email, age) 
+        customer_tbl(cust_fname,cust_mname, cust_lname, address_house_no,address_street_name,address_admin_district,address_city, cust_contact_no, cust_email, cust_age, cust_gender) 
         values("${req.body.firstname}","${req.body.middlename}","${req.body.lastname}", 
-          "${req.body.house_no}","${req.body.street_name}","${req.body.brgy_district}","${req.body.city}", "${req.body.contact_no}","${req.body.email}","${req.body.age}")
+          "${req.body.house_no}","${req.body.street_name}","${req.body.brgy_district}","${req.body.city}", "${req.body.contact_no}","${req.body.email}","${req.body.age}", "Male")
             `
       db.query(query, (err, out) => {
         // res.redirect('/adminCustomer')
         console.log(query)
+        console.log(out.insertId)
       })
     })
     // ===============================================
@@ -77,15 +111,15 @@ router.get('/bookService', (req, res) => {
     router.post('/adminCustomer/update', (req, res) => {
 
       const query = `UPDATE customer_tbl set 
-      cust_firstname="${req.body.cust_firstname}",
-      cust_middlename="${req.body.cust_middlename}",
-      cust_lastname="${req.body.cust_lastname}",
-      house_no="${req.body.house_no}",
-      street_name="${req.body.street_name}",
-      brgy_district="${req.body.brgy_district}",
-      city="${req.body.city}",
-      age="${req.body.age}",
-      contact_no="${req.body.contact_no}",
+      cust_fname="${req.body.cust_fname}",
+      cust_mname="${req.body.cust_mname}",
+      cust_lname="${req.body.cust_lname}",
+      address_house_no="${req.body.address_house_no}",
+      address_street_name="${req.body.address_street_name}",
+      address_admin_district="${req.body.address_admin_district}",
+      address_city="${req.body.address_city}",
+      cust_age="${req.body.cust_age}",
+      cust_contact_no="${req.body.cust_contact_no}",
       cust_email="${req.body.cust_email}"
       WHERE cust_id = ${req.body.id1}
       `
@@ -113,56 +147,83 @@ router.get('/bookService', (req, res) => {
 
     //READ
     router.get('/adminPromos', (req, res) => {
-        const query = ` select * from promo_bundle_tbl;
+        const query = ` select promo_bundle_tbl.*, service_in_promo_tbl.service_id, services_tbl.service_name from promo_bundle_tbl 
+        join service_in_promo_tbl on promo_bundle_tbl.promobundle_id = service_in_promo_tbl.promobundle_id 
+        join services_tbl on services_tbl.service_id = service_in_promo_tbl.service_id group by promo_bundle_tbl.promobundle_id;
         select * from services_tbl `
         db.query(query,(err,out) =>{
           res.render('home/views/adminPromos',{
             promos: out[0],
             services: out[1]
           })
-          console.log(out[0])
-          console.log(out[1])
         })
     })
     // ===============================================
     // CREATE(ADD)
     router.post('/adminPromos',(req, res) => {
+        var aydi;
+        console.log(req.body.promos)
         const query = `
           insert into 
-          promo_bundle_tbl(promobundle_name, services_included, duration, promobundle_price,promobundle_valid)
-          value("${req.body.name}","${req.body.promos}","${req.body.duration}","${req.body.price}","${req.body.valid}")`
-        db.query(query, (err, out) => {
-          // res.redirect('/adminPromos')
-          console.log(query)
-        })
+          promo_bundle_tbl(promobundle_name, promobundle_price,promobundle_validity, promobundle_availability, promobundle_duration, promobundle_amenity_usage)
+          value("${req.body.name}","${req.body.price}","${req.body.valid}",0 ,"${req.body.duration}","${req.body.amenity}")`
+          
+          db.query(query, (err, out) => {
+            console.log(query)
+            console.log(out.insertId)
+            aydi=out.insertId;
+            for(var i=0;i<req.body.promos.length;i++)
+            {
+              db.query(`insert into service_in_promo_tbl(promobundle_id, service_id) value("${aydi}","${req.body.promos[i]}")`, (err,out)=>{
+                console.log(req.body.promos[i])
+              })
+            }
+          })
       })
     // ===============================================
     // DELETE
     router.post('/adminPromos/delete', (req, res) => {
         console.log(req.body.id)
-        const query = `delete from promo_bundle_tbl where promobundle_id = ${req.body.id}`
+        const query = `delete from service_in_promo_tbl where promobundle_id= ${req.body.id};
+        delete from promo_bundle_tbl where promobundle_id = ${req.body.id} `
           
         db.query(query,(err,out)=>{
           res.redirect('/adminPromos')
+          console.log(query)
         })
       })
     // ===============================================
     // UPDATE
     router.post('/adminPromos/update', (req, res) => {
-
+      console.log(req.body)
+      
       const query = `UPDATE promo_bundle_tbl set
       promobundle_name="${req.body.promobundle_name}",
-      services_included="${req.body.services_included}",
-      duration="${req.body.duration}",
       promobundle_price="${req.body.promobundle_price}",
-      promobundle_status="${req.body.promobundle_status}",
-      promobundle_valid="${req.body.promobundle_valid}"
-      WHERE promobundle_id = ${req.body.id1}
+      promobundle_validity="${req.body.promobundle_validity}",
+      promobundle_availability="${req.body.promobundle_availability}",
+      promobundle_duration="${req.body.promobundle_duration}",
+      promobundle_amenity_usage="${req.body.promobundle_amenity_usage}"
+      WHERE promobundle_id = ${req.body.id1};
+      delete from service_in_promo_tbl where promobundle_id= ${req.body.id1}
       `
       db.query(query,(err,out) =>{
-          if(err) return console.log(err)
+        console.log(req.body.services_included)
+        console.log(query)
+        aydi=req.body.id1;
+        console.log("DITO ID"),console.log(aydi)
+        for(var i=0;i<req.body.services_included.length;i++)
+        {
+          db.query(`insert into service_in_promo_tbl(promobundle_id, service_id) value("${aydi}","${req.body.services_included[i]}")`, (err,out)=>{
+            console.log(req.body.services_included[i])
+            console.log("QUERY1")
+            console.log(query)
+            if(err) return console.log(err)
+          })
+        }
           res.redirect("/adminPromos")
-    })
+        if(err) return console.log(err)
+  })
     })
     router.post('/adminPromos/query',(req, res) => {
       const query = `select * from promo_bundle_tbl where promobundle_id=?`
@@ -175,9 +236,18 @@ router.get('/bookService', (req, res) => {
           // console.log(out[0])
           // console.log(req.body.id1)
       })
+    }) 
+    router.post('/adminPromos/query1',(req, res) => {
+      const query = `SELECT services_tbl.service_name, service_in_promo_tbl.service_id 
+      from services_tbl join service_in_promo_tbl on services_tbl.service_id = service_in_promo_tbl.service_id 
+      where service_in_promo_tbl.promobundle_id =?`
+      db.query(query,[req.body.id1],(err, out) => {
+        return res.send({out1:out})
+        console.log(out1)
+      })
     })
 
-
+    
 
 
 
@@ -254,10 +324,10 @@ router.get('/bookService', (req, res) => {
 
     //READ
     router.get('/adminServices', (req, res) => {
-        const query = ` select services_tbl.*, service_type.* from services_tbl, service_type 
+        const query = ` select services_tbl.*, service_type_tbl.* from services_tbl, service_type_tbl 
         where 
-        services_tbl.service_type = service_type.service_type_id;
-        select * from service_type`
+        services_tbl.service_type_id = service_type_tbl.service_type_id;
+        select * from service_type_tbl`
         db.query(query,(err,out) =>{
           res.render('home/views/adminServices',{
             services: out[0],
@@ -271,8 +341,8 @@ router.get('/bookService', (req, res) => {
     router.post('/adminServices',(req, res) => {
         const query = `
           insert into 
-          services_tbl(service_name, service_type, service_duration, service_price)
-          value("${req.body.name}","${req.body.type}","${req.body.duration}","${req.body.price}")`
+          services_tbl(service_name, service_type_id, service_duration, service_price, service_availability)
+          value("${req.body.name}","${req.body.type}","${req.body.duration}","${req.body.price}", 0)`
         db.query(query, (err, out) => {
           // res.redirect('/adminServices')
           console.log(query)
@@ -337,9 +407,34 @@ router.get('/bookService', (req, res) => {
     router.post('/adminTherapist',(req, res) => {
       const query = `
         insert into 
-        therapist_tbl(first_name,middle_name,last_name,age,house_no,street_name,brgy_district,city,contact_no,email,specialty)
-        value("${req.body.firstname}","${req.body.middlename}","${req.body.lastname}","${req.body.age}","${req.body.house_no}",
-      "${req.body.street_name}","${req.body.brgy_district}","${req.body.city}","${req.body.contact_no}","${req.body.email}","${req.body.specialty}")`
+        therapist_tbl
+        (therapist_fname, 
+        therapist_mname, 
+        therapist_lname, 
+        address_house_no, 
+        address_street_name, 
+        address_admin_district, 
+        address_city, 
+        therapist_contact_no, 
+        therapist_age, 
+        therapist_gender, 
+        therapist_email, 
+        therapist_specialty, 
+        therapist_availability)
+        value
+        ("${req.body.therapist_fname}", 
+        "${req.body.therapist_mname}", 
+        "${req.body.therapist_lname}", 
+        "${req.body.address_house_no}", 
+        "${req.body.address_street_name}", 
+        "${req.body.address_admin_district}", 
+        "${req.body.address_city}", 
+        "${req.body.therapist_contact_no}", 
+        "${req.body.therapist_age}", 
+        "Male", 
+        "${req.body.therapist_email}", 
+        "${req.body.therapist_specialty}", 
+        0)`
       db.query(query, (err, out) => {
         // res.redirect('/adminTherapist')
         console.log(query)
@@ -360,16 +455,16 @@ router.get('/bookService', (req, res) => {
     router.post('/adminTherapist/update', (req, res) => {
 
       const query = `UPDATE therapist_tbl set 
-      first_name="${req.body.first_name}",
-      middle_name="${req.body.middle_name}",
-      last_name="${req.body.last_name}",
-      house_no="${req.body.house_no}",
-      street_name="${req.body.street_name}",
-      brgy_district="${req.body.brgy_district}",
-      city="${req.body.city}",
-      age="${req.body.age}",
-      contact_no="${req.body.contact_no}",
-      email="${req.body.email}"
+      therapist_fname="${req.body.therapist_fname}",
+      therapist_mname="${req.body.therapist_lname}",
+      therapist_lname="${req.body.therapist_mname}",
+      address_house_no="${req.body.address_house_no}",
+      address_street_name="${req.body.address_street_name}",
+      address_admin_district="${req.body.address_admin_district}",
+      address_city="${req.body.address_city}",
+      therapist_age="${req.body.therapist_age}",
+      therapist_contact_no="${req.body.therapist_contact_no}",
+      therapist_email="${req.body.therapist_email}"
       WHERE therapist_id = ${req.body.id1}
       `
       db.query(query,(err,out) =>{
